2b5ffde0717f982d205da080e49c6b3f2c666ab3
Andrew <xie.andrew2235@gmail.com>
2019-04-04 23:44:34 -0700

    added insert and update functions, along with get,post, and put express routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,37 @@ var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 var burger = require('../models/burger.js');
+
+// Express GET route
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      res.render('index', hbsObject);
+  });
+});
+
+// Post route
+router.post('/burgers', function(req, res) {
+    burger.insertOne([
+      'burger_name'
+    ], [
+      req.body.burger_name
+    ], function(data) {
+      res.redirect('/');
+    });
+  });
+  
+// Put route
+router.put('/burgers/:id', function(req, res) {
+    var condition = 'id = ' + req.params.id;
+  
+    burger.updateOne({
+      devoured: true
+    }, condition, function(data) {
+      res.redirect('/');
+    });
+  });
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -2,15 +2,25 @@ var orm = require('../config/orm.js');
 
 //Create the burger object
 var burger = {
-	selectAll: function (cb) {
-		orm.selectAll('burgers', function (res){
-			cb(res);
-		});
-	},
-	
-	// Create insert function
+    selectAll: function (cb) {
+        orm.selectAll('burgers', function (res) {
+            cb(res);
+        });
+    },
 
+    // Create insert function
+    // Cols and vals are an array, while objColVals is an object specifying columns as object keys
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, function (res) {
+            cb(res);
+        });
+    },
     // Create update function
-}
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne('burgers', objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
 
 module.exports = burger;
