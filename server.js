b29afe7a1d1a0d8760ffb5f20632ad43373076da
Andrew <xie.andrew2235@gmail.com>
2019-04-05 15:15:52 -0700

    added orm variable which is the object for sql statement functions, and function for insert and update

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-// Dependencies
-var express = require('express');
-var app = express();
-var exphbs = require('express-handlebars');
+// Dependenies
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
 
-// Route
+// Creating express server
+var app = express();
+
+//Serve static content for the app from the "public" directory in the application directory
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+
+}));
+
+app.use(methodOverride('_method'));
+
+// Express handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+
+}));
+app.set('view engine', 'handlebars');
+
+
+//Import routes and give the server across to them
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-// Listener/Port
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
+
 app.listen(port);
+
+
+
