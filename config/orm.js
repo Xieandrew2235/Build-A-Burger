6fcca4ab3499db31c9a34c37aa21e480bc3887a0
Andrew <xie.andrew2235@gmail.com>
2019-04-05 19:10:49 -0700

    made css changes for styling

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var connection = require("../config/connection.js");
 // Import burger.js to use its database functions.
 var burger = require('../models/burger.js');
 function printQuestionMarks(num) {
